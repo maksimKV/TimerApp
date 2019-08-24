4e4ef498d16e19849ae859f4cc1e32de5e3d6cd9
Maksim Kanev <maks_kv@yahoo.com>
2019-08-24 18:07:16 +0300

    Added a functionality for changing between views

--- TimerApp.py
+++ TimerApp.py
@@ -1,89 +1,132 @@
 from tkinter import *
+#import tkinter as tk
+from tkinter import font as tkfont
 from tkinter import messagebox
-import TimerClass
 
-window = Tk()
-window.geometry("600x600")
-window.title("Timer App")
+#window = Tk()
+#window.geometry("600x600")
+#window.title("Timer app")
+#window.mainloop()
 
-breakSeconds = []
-breakMinutes = []
-intervalSeconds = []
-intervalMinutes =[]
+class TimerApp(Tk):
 
-def addInterval(separator):
-    breakLabel: Label = Label(separator, text="Enter break time", bg="white")
-    breakLabel.pack()
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
 
-    breakSecondsLabel: Label = Label(separator, text="Enter break seconds", bg="white")
-    breakSecondsLabel.pack()
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
-    breakSecondsSpinbox: Spinbox = Spinbox(separator, to=59, state="readonly")
-    breakSeconds.append(breakSecondsSpinbox)
-    breakSecondsSpinbox.pack()
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    breakMinutesLabel: Label = Label(separator, text="Enter break minutes", bg="white")
-    breakMinutesLabel.pack()
+        self.frames = {}
+        for F in (AddInterval, EditInterval):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-    breakMinutesSpinbox: Spinbox = Spinbox(separator, to=720, state="readonly")
-    breakMinutes.append(breakMinutesSpinbox)
-    breakMinutesSpinbox.pack()
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("AddInterval")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+class AddInterval(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+        self.breakSeconds = []
+        self.breakMinutes = []
+        self.intervalSeconds = []
+        self.intervalMinutes =[]
+
+        beginingLabel: Label = Label(self, text="Please enter as many sequences and intervals you like")
+        beginingLabel.pack()
+
+        intervalLabel: Label = Label(self, text="Enter interval time", bg="white")
+        intervalLabel.pack()
+
+        intervalSecondsLabel: Label = Label(self, text="Enter interval seconds", bg="white")
+        intervalSecondsLabel.pack()
+
+        intervalSecondsSpinbox: Spinbox = Spinbox(self, to=59, state="readonly")
+        self.intervalSeconds.append(intervalSecondsSpinbox)
+        intervalSecondsSpinbox.pack()
+
+        intervalMinutesLabel: Label = Label(self, text="Enter interval minutes", bg="white")
+        intervalMinutesLabel.pack()
+
+        intervalMinutesSpinbox: Spinbox = Spinbox(self, to=720, state="readonly")
+        self.intervalMinutes.append(intervalMinutesSpinbox)
+        intervalMinutesSpinbox.pack()
+
+        addIntervalButton: Button = Button(self, text="Add New Interval", command=self.addInterval)
+        addIntervalButton.pack()
+
+        saveIntervalButton: Button = Button(self, text="Save New Interval", command=self.saveInterval)
+        saveIntervalButton.pack()
+
+    def addInterval(self):
+        breakLabel: Label = Label(self, text="Enter break time", bg="white")
+        breakLabel.pack()
+
+        breakSecondsLabel: Label = Label(self, text="Enter break seconds", bg="white")
+        breakSecondsLabel.pack()
+
+        breakSecondsSpinbox: Spinbox = Spinbox(self, to=59, state="readonly")
+        self.breakSeconds.append(breakSecondsSpinbox)
+        breakSecondsSpinbox.pack()
+
+        breakMinutesLabel: Label = Label(self, text="Enter break minutes", bg="white")
+        breakMinutesLabel.pack()
+
+        breakMinutesSpinbox: Spinbox = Spinbox(self, to=720, state="readonly")
+        self.breakMinutes.append(breakMinutesSpinbox)
+        breakMinutesSpinbox.pack()
 
 
-    intervalLabel: Label = Label(separator, text="Enter interval time", bg="white")
-    intervalLabel.pack()
+        intervalLabel: Label = Label(self, text="Enter interval time", bg="white")
+        intervalLabel.pack()
 
-    intervalSecondsLabel: Label = Label(separator, text="Enter interval seconds", bg="white")
-    intervalSecondsLabel.pack()
+        intervalSecondsLabel: Label = Label(self, text="Enter interval seconds", bg="white")
+        intervalSecondsLabel.pack()
 
-    intervalSecondsSpinbox: Spinbox = Spinbox(separator, to=59, state="readonly")
-    intervalSeconds.append(intervalSecondsSpinbox)
-    intervalSecondsSpinbox.pack()
+        intervalSecondsSpinbox: Spinbox = Spinbox(self, to=59, state="readonly")
+        self.intervalSeconds.append(intervalSecondsSpinbox)
+        intervalSecondsSpinbox.pack()
 
-    intervalMinutesLabel: Label = Label(separator, text="Enter interval minutes", bg="white")
-    intervalMinutesLabel.pack()
+        intervalMinutesLabel: Label = Label(self, text="Enter interval minutes", bg="white")
+        intervalMinutesLabel.pack()
 
-    intervalMinutesSpinbox: Spinbox = Spinbox(separator, to=720, state="readonly")
-    intervalMinutes.append(intervalMinutesSpinbox)
-    intervalMinutesSpinbox.pack()
+        intervalMinutesSpinbox: Spinbox = Spinbox(self, to=720, state="readonly")
+        self.intervalMinutes.append(intervalMinutesSpinbox)
+        intervalMinutesSpinbox.pack()
 
-def saveInterval():
-    messagebox.showinfo("Interval Seconds", getIntervalSeconds())
-    #messagebox.showinfo("Title", getIntervals())
-    #w = Label(separator, text=intervals, bg="white")
-    #w.pack()
+    def saveInterval(self):
+        messagebox.showinfo("Interval Seconds", self.getIntervalSeconds(self))
+        #messagebox.showinfo("Title", getIntervals())
+        #w = Label(separator, text=intervals, bg="white")
+        #w.pack()
 
-def getIntervalSeconds():
-    values = [intervalSecondsSpinbox.get() for intervalSecondsSpinbox in intervalSeconds]
-    return values
+    def getIntervalSeconds(self):
+        values = [intervalSecondsSpinbox.get() for intervalSecondsSpinbox in self.intervalSeconds]
+        return values
 
-beginingLabel: Label = Label(window, text="Please enter as many sequences and intervals you like")
-beginingLabel.pack()
+class EditInterval(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
 
-separator: Frame = Frame(bd=1, padx=5, pady=10, relief=SUNKEN, bg="white")
-separator.pack(fill=X, padx=5, pady=5)
-
-intervalLabel: Label = Label(separator, text="Enter interval time", bg="white")
-intervalLabel.pack()
-
-intervalSecondsLabel: Label = Label(separator, text="Enter interval seconds", bg="white")
-intervalSecondsLabel.pack()
-
-intervalSecondsSpinbox: Spinbox = Spinbox(separator, to=59, state="readonly")
-intervalSeconds.append(intervalSecondsSpinbox)
-intervalSecondsSpinbox.pack()
-
-intervalMinutesLabel: Label = Label(separator, text="Enter interval minutes", bg="white")
-intervalMinutesLabel.pack()
-
-intervalMinutesSpinbox: Spinbox = Spinbox(separator, to=720, state="readonly")
-intervalMinutes.append(intervalMinutesSpinbox)
-intervalMinutesSpinbox.pack()
-
-addIntervalButton: Button = Button(separator, text="Add New Interval", command=lambda: addInterval(separator))
-addIntervalButton.pack()
-
-saveIntervalButton: Button = Button(separator, text="Save New Interval", command=saveInterval)
-saveIntervalButton.pack()
-
-window.mainloop()
+if __name__ == "__main__":
+    app = TimerApp()
+    app.mainloop()
